6cee71f64bda70551cb9123fab892e9bd3eca2b9
scouserTommy <rostyslav.skliar@gmail.com>
2013-06-19 00:53:59 +0300

    pom configuration + AppLauncher and EmptyTest to check maven properly running unit tests.

---
+++ SnBlocker/src/test/java/com/snblocker/launch/EmptyTest.java
@@ -0,0 +1,11 @@
+package com.snblocker.launch;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class EmptyTest {
+	@Test
+	public void emptyTest(){
+		Assert.assertTrue(Boolean.TRUE);
+	}
+}
